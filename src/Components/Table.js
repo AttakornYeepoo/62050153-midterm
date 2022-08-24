fe273ffdaae58d397d7ed4b04e8f0917beccbc7f
AttakornYeepoo <62050153@go.buu.ac.th>
2022-08-24 15:52:44 +0700

    first commit

---
+++ src/Components/Table.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+
+const Table = ({ products }) => {
+  const [sortProduct, setSortProduct] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const onSortClick = () => {
+    console.log("click");
+
+    const tempProducts = [...products];
+    const res = tempProducts.sort((a, b) => {
+      return a.price - b.price;
+    });
+
+    setSortProduct(res);
+
+    console.log({ res });
+  };
+  const onSortClick2 = () => {
+    console.log("click");
+
+    const tempProducts = [...products];
+    const res = tempProducts.sort((a, b) => {
+      return b.price - a.price;
+    });
+
+    setSortProduct(res);
+
+    console.log({ res });
+  };
+  const onSortClick3 = () => {
+    console.log("click");
+
+    const tempProducts = [...products];
+    const res = tempProducts.sort((a, b) => {
+      return a.stock - b.stock;
+    });
+
+    setSortProduct(res);
+  };
+  const onSortClick4 = () => {
+    console.log("click");
+
+    const tempProducts = [...products];
+    const res = tempProducts.sort((a, b) => {
+      return b.stock - a.stock;
+    });
+
+    setSortProduct(res);
+  };
+  const onSortClick5 = () => {
+    setSortProduct(products);
+  };
+  const onHandleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const tempProducts = [...products];
+
+    const filterProducts = tempProducts.filter((item) => {
+      return item.province
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
+    });
+
+    console.log({ filterProducts });
+
+    setSortProduct(filterProducts);
+  };
+
+  useEffect(() => {
+    setSortProduct(products);
+  }, [products]);
+
+  return (
+    <div>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">
+          search
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="search..."
+          value={searchTerm}
+          onChange={onHandleSearch}
+        />
+      </div>
+
+      <table className="table table-dark table-striped shadow">
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col"></th>
+            <th scope="col">จังหวัด</th>
+            <th scope="col">ผู้ติดเชื้อใหม่</th>
+            <th scope="col">ผู้เสียชีวิต</th>
+            <th scope="col">ผู้ติดเชื้อทั้งหมด</th>
+            <th scope="col">ผู้เสียชีวิตทั้งหมด</th>
+            <th scope="col">txn_date</th>
+            <th scope="col">อัพเดทล่าสุด</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortProduct.map((items, index) => {
+            return (
+              <tr className="table">
+                <td>{index + 1}</td>
+                <td>
+                  {/* <img
+                    src={items.thumbnail}
+                    alt={items.title}
+                    width="30px"
+                  ></img> */}
+                </td>
+                <td>{items.province}</td>
+
+                <td>{items.new_case}</td>
+                <td>{items.new_death}</td>
+                <td>{items.total_case}</td>
+                <td>{items.total_death}</td>
+                <td>{items.txn_date}</td>
+                <td>{items.update_date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
